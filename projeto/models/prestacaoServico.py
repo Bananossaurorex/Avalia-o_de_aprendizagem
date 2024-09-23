3babaefc0d9299bfb2079b31f006e5c838fbf1da
Bananossaurorex <160606050+Bananossaurorex@users.noreply.github.com>
2024-09-23 23:48:08 +0000

    up

--- projeto/models/prestacaoServico.py
+++ projeto/models/prestacaoServico.py
@@ -1,13 +1,15 @@
-from projeto.models.endereco import Endereco
-from projeto.models.juridica import Juridica
+from ..models.endereco import Endereco
+from ..models.juridica import Juridica
 
-class Fornecedor(Juridica):
-    def __init__(self ,id: int, nome: str, telefone: int, email: str, endereco: Endereco, cnpj: str, inscricaoEstadual: str,produto: str) -> None:
-        super().__init__(id,nome, telefone, email, endereco, cnpj, inscricaoEstadual)
-        self.produto = produto
+class PrestacaoServico(Juridica):
+    def __init__(self, nome: str, telefone: str, email: str, endereco: Endereco, cnpj: str, inscricaoEstadual: str, contratoInicio: str,contratoFim: str) -> None:
+        super().__init__(nome, telefone, email, endereco, cnpj, inscricaoEstadual)
+        self.contratoInicio = contratoInicio
+        self.contratoFim = contratoFim
 
     def __str__(self) -> str:
         return (
             f"{super().__str__()}"
-            f"\nProduto: {self.produto}"
-                )
+            f"Inicio do contrato: {self.contratoInicio}"
+            f"Final do contrato: {self.contratoFim}"
+            )
